38d4d6e53b963f9b1f7309c00ff63edb2aa07b79
cosmonot <oa.ae1010@gmail.com>
2024-04-21 12:30:28 -0500

    instantiating my new tamagotchi

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // alert("Hello World!")
 // a class for my pet
-// class Pet {
-
-// }
+class Pet {
+    constructor(name, health, ){
+        name = this.name
+        health = 10
+    }
+}
+// instantiating my new tamagotchi
+const tamagotchi = new Pet("", 10)
